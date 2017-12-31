ee891fea433eaf0ecd4f25ebf4c8a97ddb025eee
Takahiro Kobayashi <koba925@gmail.com>
2017-12-31 15:28:55 +0900

    Added Axioms of Equal(First Version) and created chapter1.example8

--- LittleProver.scm
+++ LittleProver.scm
@@ -4,14 +4,18 @@
 ;; 自分用prelude
 
 (defun my/axioms ()
-  '(
-    ; Consの公理（最初のバージョン）
+  '(; Consの公理（最初のバージョン）
     (dethm atom/cons (x y)
       (equal (atom (cons x y)) 'nil))
     (dethm car/cons (x y)
       (equal (car (cons x y)) x))
     (dethm cdr/cons (x y)
-      (equal (cdr (cons x y)) y))))
+      (equal (cdr (cons x y)) y))
+    ; Equalの公理（最初のバージョン）
+    (dethm equal-same (x)
+      (equal (equal x x) 't))
+    (dethm equal-swap (x y)
+      (equal (equal x y) (equal y x)))))
 
 (defun my/prelude ()
   (J-Bob/define (my/axioms)
@@ -100,6 +104,15 @@
    '(atom (cdr (cons (car (cons p q)) '())))
    '(((1) (cdr/cons (car (cons p q)) '()))
      (() (atom '()))))
+ 't)
+
+(my/test
+ "chapter1.example8"
+ (J-Bob/step (my/prelude)
+   '(car (cons (equal (cons x y) (cons x y)) '(and crumpets)))
+   '(((1 1) (equal-same (cons x y)))
+     ((1) (cons 't '(and crumpets)))
+     (() (car '(t and crumpets)))))
  't)
 
 (my/test/result)
