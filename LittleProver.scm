b93cf126355aeb62dd3231a25456730e5c49d5cd
Takahiro Kobayashi <koba925@gmail.com>
2018-01-06 17:41:38 +0900

    Defined the theorem of in-first-of pair.

--- LittleProver.scm
+++ LittleProver.scm
@@ -301,4 +301,19 @@
 
 (my/test/define 'defun.in-pair?)
 
+(defun dethm.in-first-of-pair ()
+  (J-Bob/define (defun.in-pair?)
+    '(((dethm in-first-of-pair (b)
+         (equal (in-pair? (pair '? b)) 't))
+       nil
+       ((1 1) (pair '? b))
+       ((1) (in-pair? (cons '? (cons b '()))))
+       ((1 Q 1) (first-of (cons '? (cons b '()))))
+       ((1 Q 1) (car/cons '? (cons b '())))
+       ((1 Q) (equal-same '?))
+       ((1) (if-true 't (equal (second-of (cons '? (cons b '()))) '?)))
+       (() (equal-same 't))))))
+
+(my/test/define 'dethm.in-first-of-pair)
+
 (my/test/result)
